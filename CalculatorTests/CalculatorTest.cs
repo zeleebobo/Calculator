d78c542f26364006d9bf3075df15906b7b056e8e
Nikita Gurzhey <zeleebobo@gmail.com>
2019-03-13 23:34:22 +0500

    Create base test

---
+++ CalculatorTests/CalculatorTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace CalculatorTests
+{
+    public class CalculatorTest
+    {
+        [Theory]
+        [InlineData("1 + 1", 2)]
+        [InlineData("0+ 3", 3)]
+        [InlineData("0 - 4", -4)]
+        public void SuccessCalculates(string input, double expected)
+        {
+            var calc = new Calculator.Calculator();
+            var result = calc.Calculate(input);
+            
+            Assert.Equal(expected, result, 5);
+        }
+    }
+}
