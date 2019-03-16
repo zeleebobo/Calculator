da70c22acef8ff71157f152f9285a7712859b1fb
Nikita Gurzhey <zeleebobo@gmail.com>
2019-03-16 22:29:31 +0500

    Add postfix evaluator implementation

--- Calculator/Evaluator/PostfixEvaluator.cs
+++ Calculator/Evaluator/PostfixEvaluator.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections.Generic;
-using Calculator.Converter;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using Calculator.ExpressionElements;
-using Calculator.Parser;
 
 namespace Calculator.Evaluator
 {
@@ -9,7 +9,32 @@ namespace Calculator.Evaluator
     {
         public Number Evaluate(MathExpression expression)
         {
-            throw new System.NotImplementedException();
+            var elementsStack = new Stack<ExpressionElement>();
+
+            foreach (var expressionElement in expression)
+            {
+                if (expressionElement is Number number)
+                {
+                    elementsStack.Push(number);
+                }
+                else if (expressionElement is Operator expressionOperator)
+                {
+                    var operand2 = elementsStack.Pop() as Number;
+                    var operand1 = elementsStack.Pop() as Number;
+                    elementsStack.Push(expressionOperator.Evaluate(operand1, operand2));
+                }
+                else if (expressionElement is Function expressionFunction)
+                {
+                    var parameter = elementsStack.Pop() as Number;
+                    elementsStack.Push(expressionFunction.Evaluate(parameter));
+                }
+                else
+                {
+                    throw new EvaluateException($"Unsupported element \"{expressionElement.GetType()}\" in postfix expression");
+                }
+            }
+            
+            return elementsStack.Pop() as Number;
         }
     }
 }
