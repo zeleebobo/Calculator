0400bc232a288c40566c129d913c12377c24c656
Nikita Gurzhey <zeleebobo@gmail.com>
2019-03-16 22:28:27 +0500

    Add tests for postfix evaluator

--- CalculatorTests/PostfixEvaluatorTest.cs
+++ CalculatorTests/PostfixEvaluatorTest.cs
@@ -1,6 +1,8 @@
-﻿using Calculator;
+﻿using System.Runtime.InteropServices.ComTypes;
+using Calculator;
 using Calculator.Evaluator;
 using Calculator.ExpressionElements;
+using Calculator.ExpressionElements.Functions;
 using Calculator.ExpressionElements.Operators;
 using Xunit;
 
@@ -36,6 +38,38 @@ namespace CalculatorTests
             var postfixEvaluator = new PostfixEvaluator();
             var result = postfixEvaluator.Evaluate(postfixExpression);
             Assert.Equal(6, result.Value, 3);
+        }
+        
+        [Fact]
+        public void PostfixEvaluationWithFunctions1()
+        {
+            var postfixExpression = new MathExpression()
+            {
+                new Number(0),
+                new Sine(),
+                new Number(0),
+                new Cosine(),
+                new Addition()
+            };
+            var postfixEvaluator = new PostfixEvaluator();
+            var result = postfixEvaluator.Evaluate(postfixExpression);
+            Assert.Equal(1, result.Value, 3);
+        }
+        
+        [Fact]
+        public void PostfixEvaluationWithFunctions2()
+        {
+            var postfixExpression = new MathExpression()
+            {
+                new Number(60),
+                new Cosine(),
+                new Number(30),
+                new Sine(),
+                new Addition()
+            };
+            var postfixEvaluator = new PostfixEvaluator();
+            var result = postfixEvaluator.Evaluate(postfixExpression);
+            Assert.Equal(-1.9404446, result.Value, 3);
         }
     }
 }
