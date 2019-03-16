1a1658af498b609c3e99e1dfca3883772169f668
Nikita Gurzhey <zeleebobo@gmail.com>
2019-03-16 23:59:34 +0500

    Add sine function

---
+++ Calculator/ExpressionElements/Functions/Sine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculator.ExpressionElements.Functions
+{
+    public class Sine : Function
+    {
+        public override Number Evaluate(Number parameter)
+        {
+            return new Number(Math.Sin(parameter.Value));
+        }
+    }
+}
