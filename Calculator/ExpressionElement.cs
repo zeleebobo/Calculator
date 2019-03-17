5cd92f70d343a2e556f8f89039c78808a5a6e35e
Nikita Gurzhey <zeleebobo@gmail.com>
2019-03-17 22:57:45 +0500

    Add equals and gethashcode implementation for ExpressionElement class

--- Calculator/ExpressionElement.cs
+++ Calculator/ExpressionElement.cs
@@ -5,6 +5,14 @@ namespace Calculator
 {
     public abstract class ExpressionElement
     {
-        
+        public override bool Equals(object obj)
+        {
+            return GetType() == obj.GetType();
+        }
+
+        public override int GetHashCode()
+        {
+            return GetType().GetHashCode();
+        }
     }
 }
