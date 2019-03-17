696b414558ee3195951298b5d4b4a4cce57128d1
Nikita Gurzhey <zeleebobo@gmail.com>
2019-03-17 23:01:04 +0500

    Add more tests for parser

--- CalculatorTests/StringParserTest.cs
+++ CalculatorTests/StringParserTest.cs
@@ -1,6 +1,8 @@
 ﻿using Xunit;
 using Calculator;
 using Calculator.ExpressionElements;
+using Calculator.ExpressionElements.Constants;
+using Calculator.ExpressionElements.Functions;
 using Calculator.ExpressionElements.Operators;
 using Calculator.Parser;
 
@@ -33,6 +35,45 @@ namespace CalculatorTests
                 new Number(1),
                 new Addition(),
                 new Number(2),
+                new Addition(),
+                new Number(3)
+            };
+            
+            var parser = new StringParser();
+            var actualExpression = parser.ParseElements(inputString);
+            Assert.Equal(expectedExpression, actualExpression);
+        }
+
+        [Fact]
+        public void ParsingWithFunction1()
+        {
+            var inputString = "1 + cos ( 1 + 3 )";
+            var expectedExpression = new MathExpression()
+            {
+                new Number(1),
+                new Addition(),
+                new Cosine(),
+                new OpenBracket(),
+                new Number(1),
+                new Addition(),
+                new Number(3),
+                new CloseBracket()
+            };
+            
+            var parser = new StringParser();
+            var actualExpression = parser.ParseElements(inputString);
+            Assert.Equal(expectedExpression, actualExpression);
+        }
+
+        [Fact]
+        public void ParsingWithConstant1()
+        {
+            var inputString = "PI + e + 3";
+            var expectedExpression = new MathExpression()
+            {
+                new PI(),
+                new Addition(),
+                new E(),
                 new Addition(),
                 new Number(3)
             };
