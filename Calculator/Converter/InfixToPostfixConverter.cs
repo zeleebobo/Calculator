1f014921e103737f6cbaaf0da2ff67514b2afbd4
Nikita Gurzhey <zeleebobo@gmail.com>
2019-03-18 00:59:42 +0500

    Add partially working implementation of infix to postfix converter

--- Calculator/Converter/InfixToPostfixConverter.cs
+++ Calculator/Converter/InfixToPostfixConverter.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Calculator.ExpressionElements;
 using Calculator.Parser;
 
 namespace Calculator.Converter
@@ -7,7 +9,56 @@ namespace Calculator.Converter
     {
         public MathExpression Convert(MathExpression convertingExpression)
         {
-            throw new System.NotImplementedException();
+            var postfixExpression = new MathExpression();
+            var bufferStack = new Stack<ExpressionElement>();
+            
+            foreach (var expressionElement in convertingExpression)
+            {
+                switch (expressionElement)
+                {
+                    case Number _:
+                        postfixExpression.Add(expressionElement);
+                        break;
+                    case Operator _:
+                    case Function _:
+                        if (bufferStack.Count == 0 || bufferStack.Peek() is OpenBracket)
+                        {
+                            bufferStack.Push(expressionElement);
+                        }
+                        else if (expressionElement is Function || 
+                                 (bufferStack.Peek() as Operator).Priority > (expressionElement as Operator).Priority)
+                        {
+                            bufferStack.Push(expressionElement);
+                        }
+                        else
+                        {
+                            while (!(bufferStack.Peek() is OpenBracket) ||
+                                   (bufferStack.Peek() as Operator).Priority < (expressionElement as Operator).Priority)
+                            {
+                                postfixExpression.Add(bufferStack.Pop());
+                            }
+                            bufferStack.Push(expressionElement);
+                        }
+                        break;
+                    case OpenBracket _:
+                        bufferStack.Push(expressionElement);
+                        break;  
+                    case CloseBracket _:
+                        while (!(bufferStack.Peek() is OpenBracket))
+                        {
+                            postfixExpression.Add(bufferStack.Pop());
+                        }
+                        bufferStack.Pop();
+                        break;
+                }
+            }
+
+            while (bufferStack.Count > 0)
+            {
+                postfixExpression.Add(bufferStack.Pop());
+            }
+
+            return postfixExpression;
         }
     }
 }
