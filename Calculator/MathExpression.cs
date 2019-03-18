818f94e93927199db50394b6524d9ec3b9fccd55
Nikita Gurzhey <zeleebobo@gmail.com>
2019-03-18 23:19:39 +0500

    Add ToString realisation for MathExpression

--- Calculator/MathExpression.cs
+++ Calculator/MathExpression.cs
@@ -1,9 +1,38 @@
 ﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Calculator.ExpressionElements;
 
 namespace Calculator
 {
     public class MathExpression : List<ExpressionElement>
     {
-        
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+                
+            foreach (var expressionElement in this)
+            {
+                if (expressionElement is Number number)
+                {
+                    stringBuilder.Append(number.Value);
+                }
+                else
+                {
+                    var stringDefinitionAttribute =
+                        expressionElement.GetType().GetCustomAttribute<StringDefinitionAttribute>();
+                    if (stringDefinitionAttribute != null)
+                    {
+                        stringBuilder.Append(stringDefinitionAttribute.Value);
+                    }
+                    else
+                    {
+                        throw new CustomAttributeFormatException("Some of expression elements hasn't string definition attribute");
+                    }
+                }
+                stringBuilder.Append(" ");
+            }
+            return stringBuilder.ToString();
+        }
     }
 }
