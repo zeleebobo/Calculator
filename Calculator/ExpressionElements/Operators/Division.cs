119817d126c6c73519ec6e8876798449d62ce3ee
Nikita Gurzhey <zeleebobo@gmail.com>
2019-03-19 12:11:45 +0500

    Add checkout for division by zero

--- Calculator/ExpressionElements/Operators/Division.cs
+++ Calculator/ExpressionElements/Operators/Division.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Data;
+
 namespace Calculator.ExpressionElements.Operators
 {
     [StringDefinition("/")]
@@ -6,6 +9,8 @@ namespace Calculator.ExpressionElements.Operators
         public override int Priority => 3;
         public override Number Evaluate(Number firstOperand, Number secondOperand)
         {
+            if (Math.Abs(secondOperand.Value) < 0.00000000000001)
+                throw new EvaluateException("Division by zero");
             return new Number(firstOperand.Value / secondOperand.Value);
         }
     }
