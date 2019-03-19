d4d3b9654be38931c5d184dea090619782f454fb
Nikita Gurzhey <zeleebobo@gmail.com>
2019-03-19 12:09:11 +0500

    Add abstract Validate method for expression element

--- Calculator/ExpressionElement.cs
+++ Calculator/ExpressionElement.cs
@@ -5,6 +5,6 @@ namespace Calculator
 {
     public abstract class ExpressionElement
     {
-        
+        public abstract void Validate(ExpressionElement previousElement, ExpressionElement nextElement);
     }
 }
